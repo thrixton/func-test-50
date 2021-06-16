c97fb23118f36707d87e4070bb79919abc81e4b5
John Baro <johnbaro@gmail.com>
2021-06-16 10:46:28 +1000

    IC

---
+++ QueueTrigger1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class QueueTrigger1
+    {
+        [Function("QueueTrigger1")]
+        public static void Run([QueueTrigger("myqueue-items", Connection = "odedevresourcesa393c3a_STORAGE")] string myQueueItem,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger("QueueTrigger1");
+            logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
